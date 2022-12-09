cf84de2a5119634c53e9fa71fff2d429cf86418b
tsiguenz <tsiguenz@student.42.fr>
2022-12-09 19:37:41 +0100

    first appear of wordpress

--- srcs/requirements/wordpress/wp-config.php
+++ srcs/requirements/wordpress/wp-config.php
@@ -29,7 +29,7 @@ define('DB_USER', getenv('DB_USER'));
 define('DB_PASSWORD', getenv('DB_PASSWORD'));
 
 /** Database hostname */
-define('DB_HOST', getenv('DB_HOST'));
+define('DB_HOST', "mariadb:3306");
 
 /** Database charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8' );
