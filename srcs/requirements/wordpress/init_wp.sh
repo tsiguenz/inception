164c4c1a68c3119deeda67be46cf328ce0125935
tsiguenz <tsiguenz@student.42.fr>
2022-12-14 13:29:34 +0100

    add basic user

--- srcs/requirements/wordpress/init_wp.sh
+++ srcs/requirements/wordpress/init_wp.sh
@@ -6,7 +6,8 @@ if [[ ! -f "wp-config.php" ]]
 then
 	mv /wp-config.php .
 	wp core download --allow-root
-	wp core install --url="tsiguenz.42.fr" --title="$WP_TITLE" --admin_user="$WP_ADMIN" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_MAIL" --allow-root
+	wp core install --allow-root --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_MAIL
+	wp user create --allow-root $WP_USER $WP_USER_MAIL --role=author --user_pass=$WP_USER_PASSWORD
 	chown -R www-data:www-data .
 fi
 
